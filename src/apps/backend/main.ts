10abd3b881f659780727ecbdf86455c4361673b6
Samuel Verdejo <sverdejot@sverdehost>
2023-08-15 22:30:55 +0200

    docs(backend): :memo: added swagger build (basic)
    installed nestjs swagger module and instantiated it on app bootstrap

--- src/apps/backend/main.ts
+++ src/apps/backend/main.ts
@@ -3,17 +3,30 @@ import { AppModule } from './app.module';
 import * as compression from 'compression';
 import helmet from 'helmet';
 import WinstonLogger from './winston.logger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+
   app.useLogger(app.get(WinstonLogger));
   app.use(compression());
   app.use(helmet.xssFilter());
   app.use(helmet.noSniff());
   app.use(helmet.hidePoweredBy());
   app.use(helmet.frameguard({ action: 'deny' }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cube Timer')
+    .setDescription("A Rubik's Cube Timer app")
+    .setVersion('1.0')
+    .addTag('timer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
