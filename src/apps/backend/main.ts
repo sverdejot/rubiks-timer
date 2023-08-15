e04d4063dc82293029ac5e0285e4301c10d9924e
Samuel Verdejo <sverdejot@sverdehost>
2023-08-15 15:04:20 +0200

    feat(backend): :lock: helmet middlewares
    added xssFilter, sniffing, powered-by and frameguard controls

--- src/apps/backend/main.ts
+++ src/apps/backend/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
+  app.use(helmet.xssFilter());
+  app.use(helmet.noSniff());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.frameguard({ action: 'deny' }));
   await app.listen(3000);
 }
 bootstrap();
