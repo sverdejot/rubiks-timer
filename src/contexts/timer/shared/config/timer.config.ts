4612963b0f5e441c595c110e1c820e18d7fec973
Samuel Verdejo <sverdejot@sverdehost>
2023-08-16 23:08:44 +0200

    feat(timer): :card_file_box: reading from remote database (including loading configuration)
    modified dataSourceFactory to read from file

--- src/contexts/timer/shared/config/timer.config.ts
+++ src/contexts/timer/shared/config/timer.config.ts
@@ -1,21 +1,11 @@
 import { registerAs } from '@nestjs/config';
-//import { readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 
-//const TYPEORM_CONFIG_FILE = 'typeorm.config.yaml';
+const TYPEORM_CONFIG_FILE = 'typeorm.config.yaml';
 
 const typeormConfig = yaml.load(
-  `
-db:
-sqlite:
-  database: ':memory:'
-mysql:
-  host: localhost
-  port: 3306
-  user: root
-  password: root
-  db_name: timer
-`,
+  readFileSync(`${__dirname}/persistance/${TYPEORM_CONFIG_FILE}`, 'utf-8'),
 ) as Record<string, any>;
 
 export default registerAs('timer', () => ({
